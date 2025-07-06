e7f563dc8604873d12b425b86a0990342ce7d0a7
Souvik Daw <souvikdaw2000@gmail.com>
2025-07-06 13:01:04 +0530

    100 data

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fs1hR6j6Uoj40GGG5GPb2NI6x6vwY4tdnNLj1s6Ryq8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
